d6f1b263c68dd1802bb33f88f172b0037c9c1f96
kotek <kotekmath@gmail.com>
2018-04-28 12:57:10 +0300

    1st step

--- uthreads.cpp
+++ uthreads.cpp
@@ -1,35 +1,107 @@
+#include <cstdio>
+#include <csetjmp>
+#include <csignal>
+#include <unistd.h>
+#include <sys/time.h>
+#include <vector>
 #include "uthreads.h"
+using namespace std;
 
-//TODO: do we need to make it a class?
-
-// Data members:
+// Constants:
 // ----------------
-int quantom_size_usecs;
-
+enum State {READY, WAITING, BLOCKED};
+#define JB_SP 6
+#define JB_PC 7
+typedef unsigned long address_t;
 // ----------------
+
+// global/static variables
+// ----------------
+int globalThreadCounter = 1;
+int quantomSizeUsec;
+int totalSizeOfQuantoms;
+vector <Thread> readyList; // represents readyQueue
+vector <Thread> blockedList; // represents blockedListOfThreads
+// ----------------
+
+// data structures:
+// ----------------
+struct Thread {
+    int id;
+    State state;
+    sigjmp_buf env{};
+    vector <Thread*> dependencyThList;
+
+    Thread(int id, State state){
+        this->id = id;
+        this->state = state;
+    }
+
+};
+// ----------------
+
+// helper functions:
+// ----------------
+namespace helperFuncs{
+    address_t translate_address(address_t addr)
+    {
+        address_t ret;
+        asm volatile("xor    %%fs:0x30,%0\n"
+                "rol    $0x11,%0\n"
+        : "=g" (ret)
+        : "0" (addr));
+        return ret;
+    }
+    // add another helper funcs here
+}
+// ----------------
+
 
 /*
- * Description: This function initializes the thread library.
- * You may assume that this function is called before any other thread library
+ * Description: This function initializes the Thread library.
+ * You may assume that this function is called before any other Thread library
  * function, and that it is called exactly once.
 */
 int uthread_init(int quantum_usecs){
     if (quantum_usecs < 0) return -1;
 
-    quantom_size_usecs = quantum_usecs;
+    quantomSizeUsec = quantum_usecs;
 
 }
 
+Thread initThread(void (*f), int threadId){
+    address_t sp, pc;
+    char stack1[STACK_SIZE];
+
+    Thread newTh (threadId, READY);
+    sp = (address_t)stack1 + STACK_SIZE - sizeof(address_t);
+    pc = (address_t)f;
+
+    sigsetjmp(newTh.env, 1);
+    (newTh.env->__jmpbuf)[JB_SP] = helperFuncs::translate_address(sp);
+    (newTh.env->__jmpbuf)[JB_PC] = helperFuncs::translate_address(pc);
+    sigemptyset(&newTh.env->__saved_mask);
+
+    return newTh;
+}
 /*
- * Description: This function creates a new thread, whose entry point is the
- * function f with the signature void f(void). The thread is added to the end
+ * Description: This function creates a new Thread, whose entry point is the
+ * function f with the signature void f(void). The Thread is added to the end
  * of the READY threads list. The uthread_spawn function should fail if it
  * would cause the number of concurrent threads to exceed the limit
- * (MAX_THREAD_NUM). Each thread should be allocated with a stack of size
+ * (MAX_THREAD_NUM). Each Thread should be allocated with a stack of size
  * STACK_SIZE bytes.
- * Return value: On success, return the ID of the created thread.
+ * Return value: On success, return the ID of the created Thread.
  * On failure, return -1.
 */
 int uthread_spawn(void (*f)(void)){
+    // create new Thread - allocate control block
+    // init Thread control block and stack with register value and PC
+    // tell dispatcher that it can run the Thread: put Thread in end of ready
+    // list
+    struct Thread thread1 = initThread(f, globalThreadCounter);
+    globalThreadCounter++;
+    readyList.push_back(thread1);
+
 
 }
--- uthreads.h
+++ uthreads.h
@@ -13,8 +13,8 @@
 
 
 /*
- * Description: This function initializes the thread library.
- * You may assume that this function is called before any other thread library
+ * Description: This function initializes the Thread library.
+ * You may assume that this function is called before any other Thread library
  * function, and that it is called exactly once. The input to the function is
  * the length of a quantum in micro-seconds. It is an error to call this
  * function with non-positive quantum_usecs.
@@ -23,38 +23,38 @@
 int uthread_init(int quantum_usecs);
 
 /*
- * Description: This function creates a new thread, whose entry point is the
- * function f with the signature void f(void). The thread is added to the end
+ * Description: This function creates a new Thread, whose entry point is the
+ * function f with the signature void f(void). The Thread is added to the end
  * of the READY threads list. The uthread_spawn function should fail if it
  * would cause the number of concurrent threads to exceed the limit
- * (MAX_THREAD_NUM). Each thread should be allocated with a stack of size
+ * (MAX_THREAD_NUM). Each Thread should be allocated with a stack of size
  * STACK_SIZE bytes.
- * Return value: On success, return the ID of the created thread.
+ * Return value: On success, return the ID of the created Thread.
  * On failure, return -1.
 */
 int uthread_spawn(void (*f)(void));
 
 
 /*
- * Description: This function terminates the thread with ID tid and deletes
+ * Description: This function terminates the Thread with ID tid and deletes
  * it from all relevant control structures. All the resources allocated by
- * the library for this thread should be released. If no thread with ID tid
- * exists it is considered as an error. Terminating the main thread
+ * the library for this Thread should be released. If no Thread with ID tid
+ * exists it is considered as an error. Terminating the main Thread
  * (tid == 0) will result in the termination of the entire process using
  * exit(0) [after releasing the assigned library memory].
- * Return value: The function returns 0 if the thread was successfully
- * terminated and -1 otherwise. If a thread terminates itself or the main
- * thread is terminated, the function does not return.
+ * Return value: The function returns 0 if the Thread was successfully
+ * terminated and -1 otherwise. If a Thread terminates itself or the main
+ * Thread is terminated, the function does not return.
 */
 int uthread_terminate(int tid);
 
 
 /*
- * Description: This function blocks the thread with ID tid. The thread may
- * be resumed later using uthread_resume. If no thread with ID tid exists it
+ * Description: This function blocks the Thread with ID tid. The Thread may
+ * be resumed later using uthread_resume. If no Thread with ID tid exists it
  * is considered as an error. In addition, it is an error to try blocking the
- * main thread (tid == 0). If a thread blocks itself, a scheduling decision
- * should be made. Blocking a thread in BLOCKED state has no
+ * main Thread (tid == 0). If a Thread blocks itself, a scheduling decision
+ * should be made. Blocking a Thread in BLOCKED state has no
  * effect and is not considered as an error.
  * Return value: On success, return 0. On failure, return -1.
 */
@@ -62,9 +62,9 @@ int uthread_block(int tid);
 
 
 /*
- * Description: This function resumes a blocked thread with ID tid and moves
- * it to the READY state. Resuming a thread in a RUNNING or READY state
- * has no effect and is not considered as an error. If no thread with
+ * Description: This function resumes a blocked Thread with ID tid and moves
+ * it to the READY state. Resuming a Thread in a RUNNING or READY state
+ * has no effect and is not considered as an error. If no Thread with
  * ID tid exists it is considered as an error.
  * Return value: On success, return 0. On failure, return -1.
 */
@@ -72,14 +72,14 @@ int uthread_resume(int tid);
 
 
 /*
- * Description: This function blocks the RUNNING thread until thread with
+ * Description: This function blocks the RUNNING Thread until Thread with
  * ID tid will move to RUNNING state (i.e.right after the next time that
- * thread tid will stop running, the calling thread will be resumed
- * automatically). If thread with ID tid will be terminated before RUNNING
- * again, the calling thread should move to READY state right after thread
+ * Thread tid will stop running, the calling Thread will be resumed
+ * automatically). If Thread with ID tid will be terminated before RUNNING
+ * again, the calling Thread should move to READY state right after Thread
  * tid is terminated (i.e. it won’t be blocked forever). It is considered
- * as an error if no thread with ID tid exists or if the main thread (tid==0)
- * calls this function. Immediately after the RUNNING thread transitions to
+ * as an error if no Thread with ID tid exists or if the main Thread (tid==0)
+ * calls this function. Immediately after the RUNNING Thread transitions to
  * the BLOCKED state a scheduling decision should be made.
  * Return value: On success, return 0. On failure, return -1.
 */
@@ -87,8 +87,8 @@ int uthread_sync(int tid);
 
 
 /*
- * Description: This function returns the thread ID of the calling thread.
- * Return value: The ID of the calling thread.
+ * Description: This function returns the Thread ID of the calling Thread.
+ * Return value: The ID of the calling Thread.
 */
 int uthread_get_tid();
 
@@ -105,13 +105,13 @@ int uthread_get_total_quantums();
 
 
 /*
- * Description: This function returns the number of quantums the thread with
- * ID tid was in RUNNING state. On the first time a thread runs, the function
- * should return 1. Every additional quantum that the thread starts should
- * increase this value by 1 (so if the thread with ID tid is in RUNNING state
+ * Description: This function returns the number of quantums the Thread with
+ * ID tid was in RUNNING state. On the first time a Thread runs, the function
+ * should return 1. Every additional quantum that the Thread starts should
+ * increase this value by 1 (so if the Thread with ID tid is in RUNNING state
  * when this function is called, include also the current quantum). If no
- * thread with ID tid exists it is considered as an error.
- * Return value: On success, return the number of quantums of the thread with ID tid. On failure, return -1.
+ * Thread with ID tid exists it is considered as an error.
+ * Return value: On success, return the number of quantums of the Thread with ID tid. On failure, return -1.
 */
 int uthread_get_quantums(int tid);
 
